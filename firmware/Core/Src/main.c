b96e2616183a397877fec73c14fc2347e659eff2
YTenebris-STM <yurambartsumyan@gmail.com>
2026-07-19 18:24:59 +0300

    Update main.c

--- firmware/Core/Src/main.c
+++ firmware/Core/Src/main.c
@@ -13,24 +13,25 @@ void INIT_GPIO (void);
 void INIT_TIM2 (void);
 void INIT_DMA (void);
 void INIT_BUTTON_IE (void);
+
 void StartCapture (void);
+void StopCapture (void);
+uint16_t RLE (volatile uint8_t *inbuf, uint8_t *outbuf);
+
 void SendChar (char c);
 void SendString (const char *str);
-uint16_t RLE (volatile uint8_t *inbuf, uint8_t *outbuf);
 void SendHex (uint8_t byte);
 
 
 int main ()
 {
+	// Initialization
 	INIT_RCC_72();
 	INIT_USART1();
 	INIT_GPIO();
 	INIT_TIM2();
 	INIT_DMA();
 	INIT_BUTTON_IE();
-
-
-	//SendString("LA is ready!");
 
 	while (1)
 	{
@@ -44,11 +45,7 @@ int main ()
 			{
 				SendHex(CompressedBuffer[i]);
 			}
-
-			//SendString("");
 		}
-
-		//for (int i = 0; i < 1000000; i++) {};
 
 		if (FullDataReady && CaptureRunning)
 		{
@@ -60,17 +57,13 @@ int main ()
 			{
 				SendHex(CompressedBuffer[i]);
 			}
-
-			//SendString("");
 		}
-
-		//for (int i = 0; i < 1000000; i++) {};
 	}
 
 	return 0;
 }
 
-void INIT_RCC_72 (void)
+void INIT_RCC_72 (void) // Initialization RCC on 72 MHz
 {
 	RCC->CR |= RCC_CR_HSEON;
 	while (!(RCC->CR & RCC_CR_HSERDY)) {};
@@ -99,7 +92,7 @@ void INIT_RCC_72 (void)
 	while (!(RCC->CFGR & RCC_CFGR_SWS_PLL)) {};
 }
 
-void INIT_USART1 (void)
+void INIT_USART1 (void) // Initialization USART1
 {
 
 	RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
@@ -124,7 +117,7 @@ void INIT_USART1 (void)
 
 }
 
-void INIT_GPIO (void)
+void INIT_GPIO (void) // Initialization GPIO
 {
 	// pins A0-A7 floating input
 	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
@@ -136,7 +129,7 @@ void INIT_GPIO (void)
 	GPIOA->CRL &= ~GPIO_CRL_CNF0_0;
 	GPIOA->CRL |=  GPIO_CRL_CNF0_1;
 
-	// UASRT was initialized in void INIT_USART1 (void)
+	// USART was initialized in void INIT_USART1 (void)
 
 	// Initialization LED on PB2
 	RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
@@ -152,43 +145,20 @@ void INIT_GPIO (void)
 void INIT_TIM2 (void)
 {
 	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
-	TIM2->PSC = 71;             // PSC for 1MHz
-	TIM2->ARR = 999;            // Period 1ms
-	//TIM2->DIER |= TIM_DIER_UIE; // Update Interrupt Enable
-	TIM2->DIER |= TIM_DIER_UDE;
-	//TIM2->CR2 |= TIM_CR2_MMS_1;
+	TIM2->PSC = 71;                 // PSC for 1KHz
+	TIM2->ARR = 999;                // Period 1ms
 
+	TIM2->DIER |= TIM_DIER_UDE;
+
+	// Uncomment for debugging (don't forget the handler - it is on the bottom of the file):
+
+	//TIM2->DIER |= TIM_DIER_UIE;
 	//NVIC_SetPriority(TIM2_IRQn, 0);
 	//NVIC_EnableIRQ(TIM2_IRQn);
 	//TIM2->CR1 |= TIM_CR1_CEN;
 }
-/*
-void TIM2_IRQHandler(void)
-{
-    if (TIM2->SR & TIM_SR_UIF)
-    {
-        TIM2->SR &= ~TIM_SR_UIF;
 
-
-
-
-
-        static uint32_t cnt = 0;
-        cnt++;
-
-        if (cnt >= 15)
-        {
-        	cnt = 0;
-        	GPIOB->ODR ^= (1 << 2);
-        	SendString("LED was changed!");
-        }
-
-    }
-
-}
-*/
-
-void INIT_DMA (void)
+void INIT_DMA (void) // Initialization DMA - circular mode, half/complete interrupts
 {
 	RCC->AHBENR |= RCC_AHBENR_DMA1EN;
 	DMA1_Channel2->CCR &= ~DMA_CCR_EN;
@@ -208,6 +178,99 @@ void INIT_DMA (void)
 
 }
 
+void INIT_BUTTON_IE (void) // Initialization button interrupt
+{
+	EXTI->PR  |=  EXTI_PR_PR0;
+	EXTI->IMR |=  EXTI_IMR_MR0;
+
+	AFIO->EXTICR[0] &= ~AFIO_EXTICR1_EXTI0_PA;
+	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+
+	EXTI->FTSR |= EXTI_FTSR_TR0;
+
+	NVIC_EnableIRQ(EXTI0_IRQn);
+	NVIC_SetPriority(EXTI0_IRQn, 0);
+}
+
+void StartCapture (void) // Function for start capture
+{
+	DMA1_Channel2->CCR &= ~DMA_CCR_EN;
+	while(DMA1_Channel2->CCR & DMA_CCR_EN) {};
+
+	DMA1_Channel2->CNDTR = BUFFER_SIZE;
+	HalfDataReady = 0;
+	FullDataReady = 0;
+
+	TIM2->CNT = 0;
+	DMA1_Channel2->CCR |= DMA_CCR_EN;
+	TIM2->CR1 |= TIM_CR1_CEN;
+
+	CaptureRunning = 1;
+}
+
+void StopCapture(void) // Function for stop capture
+{
+	TIM2->CR1 &= ~TIM_CR1_CEN;
+	DMA1_Channel2->CCR &= ~DMA_CCR_EN;
+	while(DMA1_Channel2->CCR & DMA_CCR_EN) {};
+
+	HalfDataReady = 0;
+	FullDataReady = 0;
+	CaptureRunning = 0;
+}
+
+uint16_t RLE (volatile uint8_t *inbuf, uint8_t *outbuf) // Function for RLE compressing
+{
+	// Format: one byte with current state data (8 pins - 8 bits) and one byte containing the current state length (offset is -1)
+	// A bit about offset: if current s
+	
+	uint16_t inbuf_idx = 0;
+	uint16_t outbuf_idx = 0;
+
+	while (inbuf_idx < BUFFER_SIZE / 2)
+	{
+		uint8_t cur_val = inbuf[inbuf_idx];
+		uint8_t cur_len = 0;
+		
+		while (inbuf_idx < (BUFFER_SIZE / 2) &&
+				cur_val == inbuf[inbuf_idx] &&
+				cur_len < (BUFFER_SIZE / 2))
+		{
+			cur_len++;
+			inbuf_idx++;
+		}
+
+		outbuf[outbuf_idx++] = cur_val;
+		outbuf[outbuf_idx++] = (cur_len - 1);
+	}
+
+	return outbuf_idx;
+}
+
+// Functions to send messages
+
+void SendChar (char c)
+{
+	while ((USART1->SR & USART_SR_TXE) == 0) {};
+	USART1->DR = c;
+}
+
+void SendString (const char *str)
+{
+	while (*str) {SendChar(*str++);};
+	SendChar('\r');
+	SendChar('\n');
+}
+
+void SendHex(uint8_t byte)
+{
+    const char hex[] = "0123456789ABCDEF";
+    SendChar(hex[(byte >> 4) & 0x0F]);
+    SendChar(hex[byte & 0x0F]);
+}
+
+// Handlers
+
 void DMA1_Channel2_IRQHandler (void)
 {
 	if (DMA1->ISR & DMA_ISR_HTIF2)
@@ -224,98 +287,6 @@ void DMA1_Channel2_IRQHandler (void)
 		FullDataReady = 1;
 		GPIOB->ODR ^= (1 << 2);
 	}
-}
-
-void StartCapture (void)
-{
-	DMA1_Channel2->CCR &= ~DMA_CCR_EN;
-	while(DMA1_Channel2->CCR & DMA_CCR_EN) {};
-
-	DMA1_Channel2->CNDTR = BUFFER_SIZE;
-	HalfDataReady = 0;
-	FullDataReady = 0;
-
-	TIM2->CNT = 0;
-	DMA1_Channel2->CCR |= DMA_CCR_EN;
-	TIM2->CR1 |= TIM_CR1_CEN;
-
-	CaptureRunning = 1;
-}
-
-void StopCapture(void)
-{
-	TIM2->CR1 &= ~TIM_CR1_CEN;
-	DMA1_Channel2->CCR &= ~DMA_CCR_EN;
-	while(DMA1_Channel2->CCR & DMA_CCR_EN) {};
-
-	HalfDataReady = 0;
-	FullDataReady = 0;
-	CaptureRunning = 0;
-}
-
-void SendChar (char c)
-{
-	while ((USART1->SR & USART_SR_TXE) == 0) {};
-	USART1->DR = c;
-}
-
-void SendString (const char *str)
-{
-	while (*str) {SendChar(*str++);};
-	SendChar('\r');
-	SendChar('\n');
-}
-
-uint16_t RLE (volatile uint8_t *inbuf, uint8_t *outbuf)
-{
-	uint16_t inbuf_idx = 0;
-	uint16_t outbuf_idx = 0;
-	//SendString("I'm in RLE (start)");
-
-	while (inbuf_idx < BUFFER_SIZE / 2)
-	{
-		uint8_t cur_val = inbuf[inbuf_idx];
-		uint8_t cur_len = 0;
-		//SendString("log0");
-
-		while (inbuf_idx < (BUFFER_SIZE / 2) &&
-				cur_val == inbuf[inbuf_idx] &&
-				cur_len < (BUFFER_SIZE / 2))
-		{
-			//SendString("log1");
-			cur_len++;
-			inbuf_idx++;
-		}
-
-		outbuf[outbuf_idx++] = cur_val;
-		outbuf[outbuf_idx++] = (cur_len - 1);
-		//SendString("log3");
-
-	}
-	//SendString("I'm in RLE (end)");
-
-	return outbuf_idx;
-}
-
-void SendHex(uint8_t byte)
-{
-    const char hex[] = "0123456789ABCDEF";
-    SendChar(hex[(byte >> 4) & 0x0F]);
-    SendChar(hex[byte & 0x0F]);
-}
-
-void INIT_BUTTON_IE (void)
-{
-	EXTI->PR  |=  EXTI_PR_PR0;
-	EXTI->IMR |=  EXTI_IMR_MR0;
-
-	AFIO->EXTICR[0] &= ~AFIO_EXTICR1_EXTI0_PA;
-	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
-
-	EXTI->FTSR |= EXTI_FTSR_TR0;
-
-	NVIC_EnableIRQ(EXTI0_IRQn);
-	NVIC_SetPriority(EXTI0_IRQn, 0);
 }
 
 void EXTI0_IRQHandler(void)
@@ -335,4 +306,24 @@ void EXTI0_IRQHandler(void)
     }
 }
 
+/*
+void TIM2_IRQHandler(void)
+{
+    if (TIM2->SR & TIM_SR_UIF)
+    {
+        TIM2->SR &= ~TIM_SR_UIF;
 
+        static uint32_t cnt = 0;
+        cnt++;
+
+        if (cnt >= 15)
+        {
+        	cnt = 0;
+        	GPIOB->ODR ^= (1 << 2);
+        	SendString("LED was changed!");
+        }
+
+    }
+
+}
+*/
